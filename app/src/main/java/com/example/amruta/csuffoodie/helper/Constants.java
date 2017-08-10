77365f8d8af17db5e82f1ad5568a016c0e721ec8
amruta-gh <amruta.ghangale7@gmail.com>
2017-08-10 15:19:51 -0700

    Update Constants.java

--- app/src/main/java/com/example/amruta/csuffoodie/helper/Constants.java
+++ app/src/main/java/com/example/amruta/csuffoodie/helper/Constants.java
@@ -3,6 +3,8 @@ package com.example.amruta.csuffoodie.helper;
 /**
  * Created by ajayc on 4/24/16.
  */
+
+// Images access 
 public class Constants {
 
     public static final String IMAGES_PATH = "com.example.amruta.csuffoodie:drawable/";
